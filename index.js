b0c560a4322067ecc03f190241769abcfffde20d
hectordiazgomez <konlaptechs@gmail.com>
2024-04-03 11:06:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const port = 5000;
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    console.log("Hola amigos");
+    res.send("Hola amigos");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
